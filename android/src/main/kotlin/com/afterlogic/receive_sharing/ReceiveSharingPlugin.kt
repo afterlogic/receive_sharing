66525c31b7478708fb486b950fedac0c1f444456
Dmitriy Usatov <usatov.dv@gmail.com>
2021-10-15 18:49:31 +0300

    updated Android dependencies

--- android/src/main/kotlin/com/afterlogic/receive_sharing/ReceiveSharingPlugin.kt
+++ android/src/main/kotlin/com/afterlogic/receive_sharing/ReceiveSharingPlugin.kt
@@ -2,44 +2,66 @@ package com.afterlogic.receive_sharing
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.media.MediaMetadataRetriever
-import android.media.ThumbnailUtils
 import android.net.Uri
-import android.provider.MediaStore
-import android.webkit.MimeTypeMap
-import androidx.core.content.MimeTypeFilter
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import org.json.JSONArray
 import org.json.JSONObject
-import java.io.File
-import java.io.FileOutputStream
 import java.net.URLConnection
 
 
-class ReceiveSharingPlugin(val registrar: Registrar) :
-        MethodCallHandler,
-        EventChannel.StreamHandler,
-        PluginRegistry.NewIntentListener {
+class ReceiveSharingPlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler,
+    PluginRegistry.NewIntentListener {
+    private lateinit var channel: MethodChannel
+    private lateinit var mediaEventChannel: EventChannel
+    private lateinit var textEventChannel: EventChannel
+    private lateinit var applicationContext: Context
 
     private var latestMedia: JSONArray? = null
-
     private var latestText: JSONArray? = null
-
     private var eventSinkMedia: EventChannel.EventSink? = null
     private var eventSinkText: EventChannel.EventSink? = null
 
-    init {
-        handleIntent(registrar.context(), registrar.activity().intent)
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "receive_sharing/messages")
+        channel.setMethodCallHandler(this)
+        mediaEventChannel =
+            EventChannel(flutterPluginBinding.binaryMessenger, "receive_sharing/events-media")
+        mediaEventChannel.setStreamHandler(this)
+        textEventChannel =
+            EventChannel(flutterPluginBinding.binaryMessenger, "receive_sharing/events-text")
+        textEventChannel.setStreamHandler(this)
+        applicationContext = flutterPluginBinding.applicationContext
     }
 
-    override fun onListen(arguments: Any?, events: EventChannel.EventSink) {
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        mediaEventChannel.setStreamHandler(null)
+        textEventChannel.setStreamHandler(null)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "getInitialMedia" -> result.success(latestMedia?.toString())
+            "getInitialText" -> result.success(latestText)
+            "reset" -> {
+                latestMedia = null
+                latestText = null
+                result.success(null)
+            }
+            else -> result.notImplemented()
+        }
+    }
+
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
         when (arguments) {
             "media" -> eventSinkMedia = events
             "text" -> eventSinkText = events
@@ -53,53 +75,13 @@ class ReceiveSharingPlugin(val registrar: Registrar) :
         }
     }
 
-    override fun onNewIntent(intent: Intent): Boolean {
-        handleIntent(registrar.context(), intent)
+    override fun onNewIntent(intent: Intent?): Boolean {
+        handleIntent(applicationContext, intent)
         return false
     }
 
-    companion object {
-        private const val MESSAGES_CHANNEL = "receive_sharing/messages"
-        private const val EVENTS_CHANNEL_MEDIA = "receive_sharing/events-media"
-        private const val EVENTS_CHANNEL_TEXT = "receive_sharing/events-text"
-
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            // Detect if we've been launched in background
-            if (registrar.activity() == null) {
-                return
-            }
-
-            val instance = ReceiveSharingPlugin(registrar)
-
-            val mChannel = MethodChannel(registrar.messenger(), MESSAGES_CHANNEL)
-            mChannel.setMethodCallHandler(instance)
-
-            val eChannelMedia = EventChannel(registrar.messenger(), EVENTS_CHANNEL_MEDIA)
-            eChannelMedia.setStreamHandler(instance)
-
-            val eChannelText = EventChannel(registrar.messenger(), EVENTS_CHANNEL_TEXT)
-            eChannelText.setStreamHandler(instance)
-
-            registrar.addNewIntentListener(instance)
-        }
-    }
-
-
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        when {
-            call.method == "getInitialMedia" -> result.success(latestMedia?.toString())
-            call.method == "getInitialText" -> result.success(latestText)
-            call.method == "reset" -> {
-                latestMedia = null
-                latestText = null
-                result.success(null)
-            }
-            else -> result.notImplemented()
-        }
-    }
-
-    private fun handleIntent(context: Context, intent: Intent) {
+    private fun handleIntent(context: Context, intent: Intent?) {
+        if (intent == null) return;
         try {
             if (intent.action == Intent.ACTION_SEND || intent.action == Intent.ACTION_SEND_MULTIPLE) {
                 val value = getMediaUris(context, intent)
